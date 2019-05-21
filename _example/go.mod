39aecd1ce3c2723189613f7f667da1a1f5e93a73
ldf <liudafang@yuansfer.com>
2019-05-21 09:04:41 +0800

    Use "GOPROXY" instead of "replace"

--- _example/go.mod
+++ _example/go.mod
@@ -2,13 +2,7 @@ module github.com/yuansfer/golang_sdk/_example
 
 go 1.12
 
-replace (
-	github.com/yuansfer/golang_sdk => ../../golang_sdk
-	golang.org/x/crypto => github.com/golang/crypto v0.0.0-20190403202508-8e1b8d32e692
-	golang.org/x/net => github.com/golang/net v0.0.0-20190403144856-b630fd6fe46b
-	golang.org/x/sys => github.com/golang/sys v0.0.0-20190403152447-81d4e9dc473e
-	golang.org/x/text => github.com/golang/text v0.3.0
-)
+replace github.com/yuansfer/golang_sdk => ../../golang_sdk
 
 require (
 	github.com/astaxie/beego v1.11.1
--- _example/main.go
+++ _example/main.go
@@ -9,11 +9,6 @@ func main() {
 	beego.BConfig.Listen.EnableHTTPS = true
 	beego.BConfig.Listen.Graceful = true
 
-	// beego.BConfig.Listen.HTTPSPort = 443
-	// beego.BConfig.Listen.HTTPSCertFile = "./server.pem"
-	// beego.BConfig.Listen.HTTPSKeyFile = "./server.key"
-	//
-	// 注册 beego 路由
 	beego.Router("/", &controllers.HomeController{})
 	beego.Router("/pay", &controllers.HomeController{})
 	beego.Router("/inquire", &controllers.InquireController{})
@@ -22,13 +17,12 @@ func main() {
 	beego.Router("/callback", &controllers.CallbackController{})
 
 	// In-store
-	//
 	beego.Router("/instore-add", &controllers.InstoreAddController{})
 	beego.Router("/instore-pay", &controllers.InstorePayController{})
 	beego.Router("/instore-qrcode", &controllers.InstoreQrcodeController{})
 	beego.Router("/instore-reverse", &controllers.InstoreReverseController{})
 	beego.Router("/micropay", &controllers.MicropayController{})
 	beego.Router("/refund", &controllers.RefundController{})
-	// 启动 beego
+
 	beego.Run()
 }
